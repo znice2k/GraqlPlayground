278ad05e4e1b760196899f30121bd9c3f85f8184
Alexander <alexander@nikiforow.com>
2020-01-17 18:54:10 +0100

    Initial Commit

---
+++ GraqlPlayground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GraqlPlayground
+//
+//  Created by Alexander Nikiforow on 17.01.20.
+//  Copyright © 2020 Alexander Nikiforow. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
